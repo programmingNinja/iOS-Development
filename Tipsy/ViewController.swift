7ec9cfc128800f07694449df80318a9d9b5dbed5
Rohan Shah <rshah4@l-sb8v17wg8w-m.homeoffice.wal-mart.com>
2015-09-01 18:03:07 -0700

    - prepared working version of the calculator - has labels and textfields - segmented menu and textfield has events - settings view need to be made

--- Tipsy/ViewController.swift
+++ Tipsy/ViewController.swift
@@ -10,9 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billAmountText: UITextField!
+    @IBOutlet weak var billLabel: UILabel!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var tipAmountLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var totalAmountLabel: UILabel!
+    @IBOutlet weak var tipPercentagesSegment: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        tipAmountLabel.text = "$0.00"
+        totalAmountLabel.text = "$0.00"
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,5 +31,32 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func onEditBillAmount(sender: AnyObject) {
+        if billAmountText.hasText() {
+            println("typing")
+            var amount = (billAmountText.text as NSString).doubleValue
+            var tip = 0.15*amount
+            var total = tip+amount
+            
+            tipAmountLabel.text = String(format: "$%.2f", tip)
+            totalAmountLabel.text = String(format: "$%.2f", total)
+            
+        }
+    }
+    @IBAction func onTap(sender: AnyObject) {
+        view.endEditing(true)
+    }
+    
+    @IBAction func onChange(sender: AnyObject) {
+        var tipPercentage = [0.15 , 0.2 , 0.25]
+        var rate = tipPercentage[tipPercentagesSegment.selectedSegmentIndex]
+        var amount = (billAmountText.text as NSString).doubleValue
+        var tip = rate * amount
+        var total = tip + amount
+        
+        tipAmountLabel.text = String(format: "$%.2f", tip)
+        totalAmountLabel.text = String(format: "$%.2f", total)
+
+    }
 }
 
