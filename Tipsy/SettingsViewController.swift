2c8c99f09f3867f9bd217c8a0f9e936df0013a2d
Rshah4 <rshah2@walmartlabs.com>
2015-09-04 00:53:10 -0700

    - removed switch, added data persistence for settings functionality

--- Tipsy/SettingsViewController.swift
+++ Tipsy/SettingsViewController.swift
@@ -10,12 +10,18 @@ import UIKit
 
 class SettingsViewController: UIViewController {
 
-    @IBOutlet weak var defSwitch: UISwitch!
     @IBOutlet weak var tipSlider: UISlider!
     @IBOutlet weak var tipLabel: UILabel!
+    var defaults = NSUserDefaults.standardUserDefaults()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        println("Settings did load")
+        defaults.setFloat(defaults.floatForKey("defaultTipPerc"), forKey: "defaultTipPerc")
+        defaults.setBool(defaults.boolForKey("useDef"), forKey: "useDef")
+        defaults.synchronize()
+        
+        tipSlider.value = defaults.floatForKey("defaultTipPerc")
         var sliderValue = Int(tipSlider.value)
         tipLabel.text = "\(sliderValue)%"
         // Do any additional setup after loading the view.
@@ -30,6 +36,8 @@ class SettingsViewController: UIViewController {
     @IBAction func onSliderChanged(sender: UISlider) {
         var sliderValue = Int(sender.value)
         tipLabel.text = "\(sliderValue)%"
+        defaults.setInteger(sliderValue, forKey: "defaultTipPerc")
+        defaults.synchronize()
     }
     /*
     // MARK: - Navigation
